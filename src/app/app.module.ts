34f03ebfe63f2dce8c42f52f394e1ebf61c7fd62
khalev <dahmed.med@gamil.com>
2020-07-23 18:29:04 +0000

    done structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+ 
+
+
+const routes: Routes = [
+{ path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'apropos', component: AboutComponent },
+]; 
+// sets up routes constant where you define your routes
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    LayoutComponent,
+    HomeComponent,
+    AboutComponent
   ],
+  
+  exports: [RouterModule],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
